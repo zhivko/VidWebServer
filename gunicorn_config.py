c5827b1676d1d8b9171eb390b901727cffff4758
zhivko <klemen.zivkovic@gmail.com>
2024-03-28 16:00:40 +0100

    change

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -12,9 +12,7 @@ bind = os.environ.get('GUNICORN_BIND', '127.0.0.1:8080')
 
 forwarded_allow_ips = '*'
 
-secure_scheme_headers = { 'X-Forwarded-Proto': 'https' }
-access-logfile = './log/access.log'
+errorlog = './log/log.log'
+loglevel = 'debug'
+accesslog = './log/access.log'
 access_log_format = '%({X-Forwarded-For}i)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"' 
-error-logfile = './log/log.log' 
-capture-output = 'True'
-log-level = 'debug'
