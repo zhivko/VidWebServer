d0912bf0176970682b17eb64579235092026174c
zhivko <klemen.zivkovic@gmail.com>
2024-03-28 15:56:41 +0100

    change

--- app.py
+++ app.py
@@ -592,7 +592,7 @@ def index():
 
 #Debug(app)
 #app = Flask(__name__)
-#app.config['DEBUG'] = True
+#app.gunicorn_config['DEBUG'] = True
 
 if __name__ == '__main__':
     app.run(host = '127.0.0.1', port = '8000', debug=True)
---
+++ gunicorn_config.py
@@ -0,0 +1,20 @@
+import os
+
+
+
+workers = int(os.environ.get('GUNICORN_PROCESSES', '2'))
+
+threads = int(os.environ.get('GUNICORN_THREADS', '4'))
+
+# timeout = int(os.environ.get('GUNICORN_TIMEOUT', '120'))
+
+bind = os.environ.get('GUNICORN_BIND', '127.0.0.1:8080')
+
+forwarded_allow_ips = '*'
+
+secure_scheme_headers = { 'X-Forwarded-Proto': 'https' }
+access-logfile = './log/access.log'
+access_log_format = '%({X-Forwarded-For}i)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"' 
+error-logfile = './log/log.log' 
+capture-output = 'True'
+log-level = 'debug'
