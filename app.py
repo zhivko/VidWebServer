5b0ee7f35f3a1ce012daec463151d6aa2acd7e96
zhivko <klemen.zivkovic@gmail.com>
2024-03-29 20:44:23 +0100

    test

--- app.py
+++ app.py
@@ -548,7 +548,7 @@ thread2 = Thread()
 
 @app.errorhandler(404)
 def page_not_found(error):
-    return render_template('404.html'), 404
+    return render_template('./404.html'), 404
 
 @app.route('/index.html')
 def index():
